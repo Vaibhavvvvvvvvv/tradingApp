46659f536e0f45678637e5ee5a04383fcecfb6cf
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-30 14:09:16 +0530

    complete_pricing_component

--- front-end/src/LandingPage/pricing/Hero.jsx
+++ front-end/src/LandingPage/pricing/Hero.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div>
-      <h1>hero</h1>
-    </div>
-  )
-}
+    <>
+      <div className='container mt-5'>
+        <div className='pt-5'>
+          <h1>Pricing</h1>
+          <h5>Free equity investments and flat ₹20 intraday and F&O trades</h5>
+        </div>
+        <div className='row border-top'>
+          <div className='col-12 col-md-4 text-center'>
+            <img src='https://zerodha.com/static/images/pricing-eq.svg' alt='Free equity delivery' className='img-fluid' />
+            <h2>Free equity delivery</h2>
+            <p>All equity delivery investments (NSE, BSE) are absolutely free — ₹ 0 brokerage.</p>
+          </div>
+          <div className='col-12 col-md-4 text-center'>
+            <img src='https://zerodha.com/static/images/other-trades.svg' alt='Intraday and F&O trades' className='img-fluid' />
+            <h2>Intraday and F&O trades</h2>
+            <p>Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+          </div>
+          <div className='col-12 col-md-4 text-center'>
+            <img src='https://zerodha.com/static/images/pricing-eq.svg' alt='Free direct MF' className='img-fluid' />
+            <h2>Free direct MF</h2>
+            <p>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
